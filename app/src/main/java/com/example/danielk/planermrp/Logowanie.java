34d0d45b21b01ca4d912a622d256d37bb451bea9
sowplvision <sowplvision@gmail.com>
2017-05-17 13:20:10 +0200

    Podstawowy zarys logowania do aplikacji

--- app/src/main/java/com/example/danielk/planermrp/Logowanie.java
+++ app/src/main/java/com/example/danielk/planermrp/Logowanie.java
@@ -2,6 +2,7 @@ package com.example.danielk.planermrp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Logowanie extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class Logowanie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logowanie);
+    }
+
+    protected void zaloguj(View view){
+        /*TODO Obsługa przycisku zaloguj:
+
+        * czy użytkownik istnieje
+        * czy zapamietac dane w preferencjach jezeli nie zapamietane
+        * logowanie do bazy
+        Ewentualnie pobieranie z bazy jakis rzeczy ktore sa tylko dla tego uzytkownika
+         */
     }
 }
