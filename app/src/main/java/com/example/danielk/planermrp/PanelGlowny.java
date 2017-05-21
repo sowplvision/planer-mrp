75da9879fb5a192cf3c64547a7caacd7156ad3fe
sowplvision <sowplvision@gmail.com>
2017-05-21 10:28:56 +0200

    Wstępny Panel główny - testowy

--- app/src/main/java/com/example/danielk/planermrp/PanelGlowny.java
+++ app/src/main/java/com/example/danielk/planermrp/PanelGlowny.java
@@ -2,6 +2,7 @@ package com.example.danielk.planermrp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PanelGlowny extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class PanelGlowny extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_glowny);
+    }
+
+    protected void stworzPlan(View view){
+        //TODO
+    }
+
+    protected void wpisDoBazy(View view){
+        //TODO
+    }
+
+    protected void informacje(View view){
+        //TODO
     }
 }
