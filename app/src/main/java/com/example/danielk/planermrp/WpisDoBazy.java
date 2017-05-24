667dc19681cacfc2a9f9b3f9068cf194c1ec7c34
sowplvision <sowplvision@gmail.com>
2017-05-24 13:30:36 +0200

    Spinner i tablica stringów

--- app/src/main/java/com/example/danielk/planermrp/WpisDoBazy.java
+++ app/src/main/java/com/example/danielk/planermrp/WpisDoBazy.java
@@ -5,13 +5,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class WpisDoBazy extends AppCompatActivity {
+public class WpisDoBazy extends AppCompatActivity implements OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wpis_do_bazy);
+
+        Spinner typProduktu = (Spinner) findViewById(R.id.typProduktu);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.typ_produktu, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        typProduktu.setAdapter(adapter);
+        typProduktu.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String typProduktu = parent.getItemAtPosition(position).toString();
+
+        if(typProduktu.equals("Materiał")){
+
+        }
+        if(typProduktu.equals("Półprodukt")){
+
+        }
+        if(typProduktu.equals("Produkt")){
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     @Override
@@ -30,4 +65,6 @@ public class WpisDoBazy extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
