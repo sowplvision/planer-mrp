b9cebd522ab2d9bd74ccf3b4bf7cf14adcef28e4
Mefedronpl <majach18now@gmail.com>
2017-05-28 00:38:39 +0200

    Tworzenie planu - pobieranie danych [popyt i produkcja]

--- app/src/main/java/com/example/danielk/planermrp/TworzeniePlanu.java
+++ app/src/main/java/com/example/danielk/planermrp/TworzeniePlanu.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -15,13 +16,72 @@ import java.util.List;
 
 public class TworzeniePlanu extends AppCompatActivity {
     private EditText wartoscPopyt1, wartoscPopyt2, wartoscPopyt3, wartoscPopyt4, wartoscPopyt5, wartoscPopyt6, wartoscPopyt7, wartoscPopyt8, wartoscPopyt9, wartoscPopyt10;
+    private EditText wartoscProdukcji1, wartoscProdukcji2, wartoscProdukcji3, wartoscProdukcji4, wartoscProdukcji5, wartoscProdukcji6, wartoscProdukcji7, wartoscProdukcji8, wartoscProdukcji9, wartoscProdukcji10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tworzenie_planu);
+        setContentView(R.layout.activity_tworzenie_planu);}
 
 
+
+    public void stworzPlan(View view) {
+        wartoscPopyt1 = (EditText)findViewById(R.id.edit_n_popyt1);
+        wartoscPopyt2 = (EditText)findViewById(R.id.edit_n_popyt2);
+        wartoscPopyt3 = (EditText)findViewById(R.id.edit_n_popyt3);
+        wartoscPopyt4 = (EditText)findViewById(R.id.edit_n_popyt4);
+        wartoscPopyt5 = (EditText)findViewById(R.id.edit_n_popyt5);
+        wartoscPopyt6 = (EditText)findViewById(R.id.edit_n_popyt6);
+        wartoscPopyt7 = (EditText)findViewById(R.id.edit_n_popyt7);
+        wartoscPopyt8 = (EditText)findViewById(R.id.edit_n_popyt8);
+        wartoscPopyt9 = (EditText)findViewById(R.id.edit_n_popyt9);
+        wartoscPopyt10 = (EditText)findViewById(R.id.edit_n_popyt10);
+
+        wartoscProdukcji1 = (EditText)findViewById(R.id.edit_n_produkcje1);
+        wartoscProdukcji2 = (EditText)findViewById(R.id.edit_n_produkcje2);
+        wartoscProdukcji3 = (EditText)findViewById(R.id.edit_n_produkcje3);
+        wartoscProdukcji4 = (EditText)findViewById(R.id.edit_n_produkcje4);
+        wartoscProdukcji5 = (EditText)findViewById(R.id.edit_n_produkcje5);
+        wartoscProdukcji6 = (EditText)findViewById(R.id.edit_n_produkcje6);
+        wartoscProdukcji7 = (EditText)findViewById(R.id.edit_n_produkcje7);
+        wartoscProdukcji8 = (EditText)findViewById(R.id.edit_n_produkcje8);
+        wartoscProdukcji9 = (EditText)findViewById(R.id.edit_n_produkcje9);
+        wartoscProdukcji10 = (EditText)findViewById(R.id.edit_n_produkcje10);
+
+
+
+        String wartosc1 = wartoscPopyt1.getText().toString(); //pobiera wartość popytu...
+        String wartosc2 = wartoscPopyt2.getText().toString();
+        String wartosc3 = wartoscPopyt3.getText().toString();
+        String wartosc4 = wartoscPopyt4.getText().toString();
+        String wartosc5 = wartoscPopyt5.getText().toString();
+        String wartosc6 = wartoscPopyt6.getText().toString();
+        String wartosc7 = wartoscPopyt7.getText().toString();
+        String wartosc8 = wartoscPopyt8.getText().toString();
+        String wartosc9 = wartoscPopyt9.getText().toString();
+        String wartosc10 = wartoscPopyt10.getText().toString(); // .
+
+        String wprodukcji1 = wartoscProdukcji1.getText().toString(); //pobieranie wartości produkcji...
+        String wprodukcji2 = wartoscProdukcji1.getText().toString();
+        String wprodukcji3 = wartoscProdukcji1.getText().toString();
+        String wprodukcji4 = wartoscProdukcji1.getText().toString();
+        String wprodukcji5 = wartoscProdukcji1.getText().toString();
+        String wprodukcji6 = wartoscProdukcji1.getText().toString();
+        String wprodukcji7 = wartoscProdukcji1.getText().toString();
+        String wprodukcji8 = wartoscProdukcji1.getText().toString();
+        String wprodukcji9 = wartoscProdukcji1.getText().toString();
+        String wprodukcji10 = wartoscProdukcji1.getText().toString(); // .
+
+
+
+
+
+    }
+
+
+
+
+        /*
         wartoscPopyt1 = (EditText)findViewById(R.id.edit_n_popyt1);
         wartoscPopyt2 = (EditText)findViewById(R.id.edit_n_popyt2);
         wartoscPopyt3 = (EditText)findViewById(R.id.edit_n_popyt3);
@@ -40,85 +100,11 @@ public class TworzeniePlanu extends AppCompatActivity {
                 String wartosc1 = wartoscPopyt1.getText().toString(); //pobiera wartość
 
             }
-        });
-        wartoscPopyt2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc2 = wartoscPopyt2.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt3.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc3 = wartoscPopyt3.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt4.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc4 = wartoscPopyt4.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt5.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc5 = wartoscPopyt5.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt6.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc6 = wartoscPopyt6.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt7.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc7 = wartoscPopyt7.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt8.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc8 = wartoscPopyt8.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt9.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc9 = wartoscPopyt9.getText().toString(); //pobiera wartość
-
-            }
-        });
-        wartoscPopyt10.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                String wartosc10 = wartoscPopyt10.getText().toString(); //pobiera wartość
-
-            }
-        });
+        }); */
 
 
 
 
-
-    }
 
 
 
